5dc2fdb4463ae956215bc2d629e68393dd4ca1c1
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2020-03-22 23:11:16 +0200

    fix LoginForm component

--- src/modules/LoginForm/components/LoginForm.jsx
+++ src/modules/LoginForm/components/LoginForm.jsx
@@ -1,11 +1,66 @@
 import React from 'react';
+import {Form, Icon, Input} from "antd";
+import {LockOutlined, UserOutlined} from "@ant-design/icons";
+import { Button, WhiteBlock } from "components";
+import {Link} from "react-router-dom";
 
-export default class App extends React.Component {
+
+
+export default class LoginForm extends React.Component {
     render() {
-        return (
-            <>
+        const onFinish = values => {
+            console.log('Received values of form: ', values);
+        };
 
-            </>
+        return (
+            <WhiteBlock>
+                <Form
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{remember: true}}
+                    onFinish={onFinish}
+                >
+                    <Form.Item
+                        hasFeedback
+                        name="username"
+                        rules={[{
+                            required: true,
+                            validateStatus: "success",
+                            message: 'Будь ласка, введіть свій Логін!'
+                        }]}
+                    >
+                        <Input
+                            prefix={<UserOutlined className="site-form-item-icon"/>}
+                            placeholder="Логін"
+                            size="large"
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        hasFeedback
+                        name="password"
+                        rules={[{
+                            required: true,
+                            validateStatus: "success",
+                            message: 'Будь ласка, введіть свій Пароль!'
+                        }]}
+                    >
+                        <Input label="Success"
+                               prefix={<LockOutlined className="site-form-item-icon"/>}
+                               type="password"
+                               placeholder="Пароль"
+                               size="large"
+                               validateStatus="success"
+                        />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" size="large" htmlType="submit">
+                            Ввійти в аккаунт
+                        </Button>
+                    </Form.Item>
+                    <Link className="auth__register-link" to="/register">Зареєструватись!</Link>
+
+                </Form>
+            </WhiteBlock>
         )
     }
 }
--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -1,73 +1,22 @@
 import React from 'react';
-import {Button, WhiteBlock} from "../../components";
-import {Form, Input, Checkbox} from 'antd';
-import {UserOutlined, LockOutlined} from '@ant-design/icons';
+import {WhiteBlock} from "../../components";
 import "./Auth.scss"
+import {LoginForm }from "modules";
 
 
-const Auth = () => {
-    const onFinish = values => {
-        console.log('Received values of form: ', values);
-    };
+const Auth = () => (
 
-    return <div className="wrapper">
+    <div className="wrapper">
         <section className="auth">
             <div className="auth__content">
                 <div className="auth__top">
                     <h2>Ввійти в аккаунт</h2>
                     <p>Будь ласка, ввійдіть в свій аккаунт</p>
                 </div>
-                <WhiteBlock>
-                    <Form
-                        name="normal_login"
-                        className="login-form"
-                        initialValues={{remember: true}}
-                        onFinish={onFinish}
-                    >
-                        <Form.Item
-                            hasFeedback
-                            name="username"
-                            rules={[{
-                                required: true,
-                                validateStatus: "success",
-                                message: 'Будь ласка, введіть свій Логін!'
-                            }]}
-                        >
-                            <Input
-                                prefix={<UserOutlined className="site-form-item-icon"/>}
-                                placeholder="Логін"
-                                size="large"
-                            />
-                        </Form.Item>
-                        <Form.Item
-                            hasFeedback
-                            name="password"
-                            rules={[{
-                                required: true,
-                                validateStatus: "success",
-                                message: 'Будь ласка, введіть свій Пароль!'
-                            }]}
-                        >
-                            <Input label="Success"
-                                   prefix={<LockOutlined className="site-form-item-icon"/>}
-                                   type="password"
-                                   placeholder="Пароль"
-                                   size="large"
-                                   validateStatus="success"
-                            />
-                        </Form.Item>
-                        <Form.Item>
-                            <Button type="primary" size="large" htmlType="submit">
-                                Ввійти в аккаунт
-                            </Button>
-                        </Form.Item>
-                            <a className="auth__register-link" href="#">Зареєструватись!</a>
-
-                    </Form>
-                </WhiteBlock>
+                    <LoginForm/>
             </div>
         </section>
-    </div>;
-};
+    </div>
+);
 
 export default Auth;
