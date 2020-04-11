cdc030c70cb58d2d330af9b37f7053300f3423a9
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2020-04-12 00:39:44 +0300

    fix Message component(add attachments line), add formik library

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -4,31 +4,43 @@ import "./Message.scss";
 import formatDistanceToNow from "date-fns/formatDistanceToNow/index";
 import ru from 'date-fns/locale/ru';
 import classNames from 'classnames';
-import readedSvg from "assets/img/readed.svg"
+import readedSvg from "assets/img/readed.svg";
+import noReadedSvg from "assets/img/noreaded.svg"
 
-const Message = ({avatar, user, text, date, isMe}) => {
+const Message = ({avatar, user, text, date, isMe, isReaded, attachments}) => {
 
     return (
         <div className={classNames('message', {'message--isme': isMe})}>
 
             <div className="message__content">
-                <img src={readedSvg} alt="Checked icon"/>
-                <div>
-                    <div className="message__avatar">
-                        <img src={avatar}
-                             alt="$`{user.fullname}` avatar"/>
+                {isMe && isReaded ? (<img className="message__icon-readed"
+                                          src={readedSvg} alt="Readed icon"/>) :
+                    (<img className="message__icon-readed message__icon-readed--no"
+                          src={noReadedSvg} alt="No readed icon"/>)}
+
+                <div className="message__avatar">
+                    <img src={avatar}
+                         alt="$`{user.fullname}` avatar"/>
+                </div>
+                <div className="message_info">
+                    <div className="message__bubble">
+                        <p className="message__text">{text}</p>
                     </div>
-                    <div className="message_info">
-                        <div className="message__bubble">
-                            <p className="message__text">{text}</p>
-                        </div>
-                        <time className="message__date">
-                            {formatDistanceToNow(new Date(date), {
-                                addSuffix: true,
-                                locale: ru
-                            })}
-                        </time>
+                    <div className="message__attachments">
+                        {attachments &&
+                        attachments.map(item => (
+                            <div className="message__attachments-item">
+                                <img src={item.url} alt={item.filename}/>
+                            </div>
+                        ))
+                        }
                     </div>
+                    <time className="message__date">
+                        {formatDistanceToNow(new Date(date), {
+                            addSuffix: true,
+                            locale: ru
+                        })}
+                    </time>
                 </div>
             </div>
         </div>
@@ -43,7 +55,8 @@ Message.propTypes = {
     avatar: propTypes.string,
     text: propTypes.string,
     user: propTypes.object,
-    date: propTypes.string
+    date: propTypes.string,
+    attachments: propTypes.array,
 };
 
 export default Message;
